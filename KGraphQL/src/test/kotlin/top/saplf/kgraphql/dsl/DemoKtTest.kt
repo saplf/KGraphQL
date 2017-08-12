9b364c738dc816d71e8750853acb7b684ff4a0ff
saplf <LimoSaplf@gmail.com>
2017-08-12 10:20:28 +0800

    Remove test code from .gitignore file.

---
+++ KGraphQL/src/test/kotlin/top/saplf/kgraphql/dsl/DemoKtTest.kt
@@ -0,0 +1,16 @@
+package top.saplf.kgraphql.dsl
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * @author saplf
+ */
+class DemoKtTest {
+    @Test
+    fun demo() {
+        top.saplf.kgraphql.dsl.demo()
+    }
+
+}
