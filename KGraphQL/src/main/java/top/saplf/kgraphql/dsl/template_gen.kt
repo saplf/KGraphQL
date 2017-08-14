a368eb9535dd52db7260632e9f8127f6c2929cc5
saplf <LimoSaplf@gmail.com>
2017-08-14 15:21:48 +0800

    Shrink tabs and spaces

--- KGraphQL/src/main/java/top/saplf/kgraphql/dsl/template_gen.kt
+++ KGraphQL/src/main/java/top/saplf/kgraphql/dsl/template_gen.kt
@@ -4,86 +4,86 @@ package top.saplf.kgraphql.dsl
  * @author saplf
  */
 class QueryNameFirst(
-        tagArgs: TagArgs = null,
-        identifier: String = "frag${System.nanoTime()}"
+    tagArgs: TagArgs = null,
+    identifier: String = "frag${System.nanoTime()}"
 ) : Tag(tagName = "First", tagArgs = tagArgs, identifier = identifier)
 
 class QueryNameSecond(
-        identifier: String = "frag${System.nanoTime()}"
+    identifier: String = "frag${System.nanoTime()}"
 ) : Tag(tagName = "Second", identifier = identifier)
 
 class QueryName(
-        tagArgs: TagArgs = null,
-        identifier: String = "frag${System.nanoTime()}",
-        init: (QueryName.() -> Unit)? = null
+    tagArgs: TagArgs = null,
+    identifier: String = "frag${System.nanoTime()}",
+    init: (QueryName.() -> Unit)? = null
 ) : BlockTag(tagName = "Name", tagArgs = tagArgs, identifier = identifier) {
 
-    init {
-        if (init != null) {
-            init.invoke(this)
-            treatAsFragment = true
-        }
+  init {
+    if (init != null) {
+      init.invoke(this)
+      treatAsFragment = true
     }
+  }
 
-    private val keyQueryNameFirstList = "list"
+  private val keyQueryNameFirstList = "list"
 
-    fun first(list: List<*>? = null) {
-        appendTag(QueryNameFirst(mapOf(keyQueryNameFirstList to list)), "first")
-    }
+  fun first(list: List<*>? = null) {
+    appendTag(QueryNameFirst(mapOf(keyQueryNameFirstList to list)), "first")
+  }
 
-    fun second() {
-        appendTag(QueryNameSecond(), "second")
-    }
+  fun second() {
+    appendTag(QueryNameSecond(), "second")
+  }
 
-    infix fun on(fragment: BlockTag) {
-        appendFragment(fragment, false)
-    }
+  infix fun on(fragment: BlockTag) {
+    appendFragment(fragment, false)
+  }
 
-    infix fun flat(fragment: BlockTag) {
-        appendFragment(fragment, true)
-    }
+  infix fun flat(fragment: BlockTag) {
+    appendFragment(fragment, true)
+  }
 
-    @Deprecated(message = "wrong scope", replaceWith = ReplaceWith("Unit"), level = DeprecationLevel.ERROR)
-    fun name(age: Int? = null, gender: String? = null, execution: QueryName.() -> Unit) = Unit
+  @Deprecated(message = "wrong scope", replaceWith = ReplaceWith("Unit"), level = DeprecationLevel.ERROR)
+  fun name(age: Int? = null, gender: String? = null, execution: QueryName.() -> Unit) = Unit
 }
 
 class Query(
-        tagArgs: TagArgs = null,
-        identifier: String = "frag${System.nanoTime()}"
+    tagArgs: TagArgs = null,
+    identifier: String = "frag${System.nanoTime()}"
 ) : BlockTag(tagName = "Query", tagArgs = tagArgs, identifier = identifier) {
 
-    fun name(age: Int? = null, gender: String? = null, execution: QueryName.() -> Unit) {
-        appendTag(tag =  QueryName(mapOf(
-                "age" to age,
-                "gender" to gender
-        )), fieldName = "name", fieldAlias = "nameAlias", execution = execution)
-    }
+  fun name(age: Int? = null, gender: String? = null, execution: QueryName.() -> Unit) {
+    appendTag(tag = QueryName(mapOf(
+        "age" to age,
+        "gender" to gender
+    )), fieldName = "name", fieldAlias = "nameAlias", execution = execution)
+  }
 
-    @Deprecated(message = "wrong scope", replaceWith = ReplaceWith("Unit"), level = DeprecationLevel.ERROR)
-    fun query(execution: Query.() -> Unit) = Unit
+  @Deprecated(message = "wrong scope", replaceWith = ReplaceWith("Unit"), level = DeprecationLevel.ERROR)
+  fun query(execution: Query.() -> Unit) = Unit
 }
 
 class GithubQueryBlock(initBlock: (GithubQueryBlock.() -> Unit)?) {
 
-    private var query: Query? = null
-    var result: String? = null
-    val fragment: BlockTag.() -> Unit
+  private var query: Query? = null
+  var result: String? = null
+  val fragment: BlockTag.() -> Unit
 
-    init {
-        fragment = {
-            val queryDump = query ?: throw RuntimeException("GraphQL's fragment should define in lazy block")
-            queryDump.defineFragment(this)
-        }
-        initBlock?.invoke(this)
+  init {
+    fragment = {
+      val queryDump = query ?: throw RuntimeException("GraphQL's fragment should define in lazy block")
+      queryDump.defineFragment(this)
     }
+    initBlock?.invoke(this)
+  }
 
-    fun query(id: String = "", execution: Query.() -> Unit) {
-        if (null != query) {
-            throw RuntimeException("You can't invoke query twice in one block.")
-        }
-        query = Query(mapOf("id" to id, "list" to listOf(1, 2, 3)))
-        val queryDump = query ?: throw RuntimeException("Some thing must be wrong.")
-        queryDump.apply(execution) // this must invoke after `query` is assigned as the `execution` will use `query`
-        result = queryDump.generate()
+  fun query(id: String = "", execution: Query.() -> Unit) {
+    if (null != query) {
+      throw RuntimeException("You can't invoke query twice in one block.")
     }
+    query = Query(mapOf("id" to id, "list" to listOf(1, 2, 3)))
+    val queryDump = query ?: throw RuntimeException("Some thing must be wrong.")
+    queryDump.apply(execution) // this must invoke after `query` is assigned as the `execution` will use `query`
+    result = queryDump.generate()
+  }
 }
--- KGraphQL/src/main/java/top/saplf/kgraphql/processor/AnnotationProcessor.kt
+++ KGraphQL/src/main/java/top/saplf/kgraphql/processor/AnnotationProcessor.kt
@@ -6,7 +6,6 @@ import top.saplf.kgraphql.annotation.KGraphQLArg
 import top.saplf.kgraphql.annotation.KGraphQLName
 import javax.annotation.processing.*
 import javax.lang.model.SourceVersion
-import javax.lang.model.element.Element
 import javax.lang.model.element.TypeElement
 import javax.lang.model.util.Elements
 
@@ -15,29 +14,29 @@ import javax.lang.model.util.Elements
  */
 @AutoService(Processor::class) class AnnotationProcessor : AbstractProcessor() {
 
-    private val classSet: Set<Class<out Annotation>> by lazy {
-        setOf(
-                KGraphQLName::class.java,
-                KGraphQLArg::class.java,
-                KGraphOperation::class.java
-        )
-    }
+  private val classSet: Set<Class<out Annotation>> by lazy {
+    setOf(
+        KGraphQLName::class.java,
+        KGraphQLArg::class.java,
+        KGraphOperation::class.java
+    )
+  }
 
-    lateinit var mElements: Elements
-    lateinit var mFiler: Filer
+  lateinit var mElements: Elements
+  lateinit var mFiler: Filer
 
-    override fun init(processingEnv: ProcessingEnvironment) {
-        super.init(processingEnv)
-        mElements = processingEnv.elementUtils
-        mFiler = processingEnv.filer
-    }
+  override fun init(processingEnv: ProcessingEnvironment) {
+    super.init(processingEnv)
+    mElements = processingEnv.elementUtils
+    mFiler = processingEnv.filer
+  }
 
-    override fun getSupportedAnnotationTypes() = classSet.map { it.canonicalName }.toSet()
+  override fun getSupportedAnnotationTypes() = classSet.map { it.canonicalName }.toSet()
 
-    override fun getSupportedSourceVersion() = SourceVersion.latestSupported()!!
+  override fun getSupportedSourceVersion() = SourceVersion.latestSupported()!!
 
-    override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment): Boolean {
+  override fun process(annotations: MutableSet<out TypeElement>?, roundEnv: RoundEnvironment): Boolean {
 
-        return false
-    }
+    return false
+  }
 }
